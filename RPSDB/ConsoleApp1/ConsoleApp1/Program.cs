0925c728132137aa96b20cdde5c72ad05edcef1b
Richard Hawkins <Richard.Hawkins@revature.com>
2021-12-17 11:51:38 -0500

    updates

---
+++ RPSDB/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Data.SqlClient;
+
+string? connectionString = File.ReadAllText("c:/Revature/DEMO_RPS_CS.txt"); 
+Console.WriteLine("Establishing DB Connection...");
+using SqlConnection connection = new(connectionString);
+connection.Open();
+Console.WriteLine("Connection Established.");
+
+using SqlCommand command = new("SELECT * FROM TEST;", connection);
+using SqlDataReader reader = command.ExecuteReader();
+while (reader.Read())
+{
+
+    string field = reader.GetString(0);
+
+    Console.WriteLine($"\"{field}\"");
+}
+connection.Close();
